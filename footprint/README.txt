015a02254e93d043e0239541b845b74c6a535920
John McMaster <johndmcmaster@gmail.com>
2018-05-23 16:03:40 -0700

    sync
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

---
+++ footprint/README.txt
@@ -0,0 +1,3 @@
+https://raw.githubusercontent.com/Kazu-zamasu/KiCAD-foot-print/master/Hobby-Devices.pretty/DCJack(EJ508A-ND).kicad_mod
+https://github.com/Kazu-zamasu/KiCAD-foot-print
+
---
+++ ilr-lm317_r1/led_arrange.py
@@ -0,0 +1,40 @@
+import math
+import pcbnew
+
+def torad(x):
+    return 3.14 * x / 180.
+
+r = 25.
+n = 24
+theta = 360. / n
+
+i = 0
+board = pcbnew.GetBoard()
+for m in list(board.m_Modules):
+    print
+    print m.GetReference()
+    if m.GetReference()[0] != 'D':
+        continue
+    if m.GetReference() == 'D1':
+        continue
+
+    print i
+    a = i * theta
+    x = r * math.sin(torad(a)) - 1.27
+    y = r * math.cos(torad(a))
+
+    xnew = x + 100
+    ynew = 100 - y
+    # in 10ths of degree
+    anew = 10 * (360 - (a + 180))
+    print 'x = %0.1f' % (xnew)
+    print 'y = %0.1f' % (ynew)
+    print 'a = %u' % anew
+
+    # in nm...we have mm
+    m.SetPosition(pcbnew.wxPoint(xnew * 1e6, ynew * 1e6))
+    m.SetOrientation(anew)
+
+    i += 1
+
+
